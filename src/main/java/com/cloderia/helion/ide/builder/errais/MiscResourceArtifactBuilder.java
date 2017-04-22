9f266e7ec32ea28078e98dac26828d0805b33d82
Edward Banfa <ebanfa@gmail.com>
2017-04-23 02:05:59 +0300

    Helion maven plugin commit

---
+++ src/main/java/com/cloderia/helion/ide/builder/errais/MiscResourceArtifactBuilder.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.cloderia.helion.ide.builder.errais;
+
+import com.cloderia.helion.ide.app.Application;
+import com.cloderia.helion.ide.builder.AbstractArtifactBuilder;
+import com.cloderia.helion.ide.configuration.BuildConfiguration;
+import com.cloderia.helion.ide.util.IDEConstants;
+import com.cloderia.helion.ide.util.IDEException;
+
+/**
+ * @author adrian
+ *
+ */
+public class MiscResourceArtifactBuilder extends AbstractArtifactBuilder {
+	
+	public static final String DDL_SQL = "ddl.sql";
+	public static final String DDL_SQL_FTL = "sql/ddl-sql.ftl";
+
+	/* (non-Javadoc)
+	 * @see com.cloderia.helion.ide.builder.ArtifactBuilder#build(com.cloderia.helion.ide.configuration.BuildConfiguration)
+	 */
+	public void build(BuildConfiguration buildConfiguration) throws IDEException {
+		String targetDir = buildConfiguration.getTargetDir();
+		Application application = buildConfiguration.getApplication();
+		this.generateArtifact(buildConfiguration, application, DDL_SQL_FTL, DDL_SQL, targetDir.concat(IDEConstants.WP_SQL_DIR));
+	}
+
+}
